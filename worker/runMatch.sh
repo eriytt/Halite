cac956d08e7aa8b263c694b6f6171c244f99053a
Erik Ytterberg <erik.ytterberg@learningwell.se>
2017-11-06 23:55:26 +0100

    Top level run script

---
+++ worker/runMatch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WORKERDIR=`realpath \`dirname $0\``
+WIDTH=$1
+HEIGHT=$2
+
+shift; shift
+
+BOTS=$*
+NUMBOTS=`echo $BOTS | wc -w`
+
+pushd $WORKERDIR
+./runGame.sh $WIDTH $HEIGHT $NUMBOTS $BOTS $BOTS
+
+cat *.hlt > visualizer_pipe
+rm *.hlt
+
+popd
+
+exit
