18fc7d1e1d84ce703608252ffc517eda737bcd2e
Erik Ytterberg <erik.ytterberg@learningwell.se>
2017-11-09 16:05:48 +0100

    Untar published packages

--- worker/getBots.sh
+++ worker/getBots.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 scp -r 'halite@well.learningwell.se:/home/halite/teams/*' /home/halite/teams
+
+for p in /home/halite/teams/*/publish.tar.gz; do
+    pushd `dirname $p`
+    tar -zxvf publish.tar.gz
+    popd
+done
